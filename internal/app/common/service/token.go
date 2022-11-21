d5c2a83f977cb6dfaf6fe6e7f958764f683e8626
Ryouji <1354413904@qq.com>
2022-11-21 15:53:13 +0800

    新增token生成接口

---
+++ internal/app/common/service/token.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"context"
+	"github.com/Rewriterl/xxManage/v1/internal/app/common/consts"
+	"github.com/Rewriterl/xxManage/v1/internal/app/common/model"
+	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/tiger1103/gfast-token/gftoken"
+)
+
+type IToken interface {
+	GenerateToken(ctx context.Context, key string, data interface{}) (keys string, err error)
+	Middleware(group *ghttp.RouterGroup) error
+	ParseToken(r *ghttp.Request) (*gftoken.CustomClaims, error)
+	IsLogin(r *ghttp.Request) (b bool, failed *gftoken.AuthFailed)
+	GetRequestToken(r *ghttp.Request) (token string)
+	RemoveToken(ctx context.Context, token string) (err error)
+}
+
+type gfTokenImpl struct {
+	*gftoken.GfToken
+}
+
+var gT = gfTokenImpl{
+	GfToken: gftoken.NewGfToken(),
+}
+
+func GfToken(options *model.TokenOptions) IToken {
+	var fun gftoken.OptionFunc
+	if options.CacheModel == consts.CacheModelRedis {
+		fun = gftoken.WithGRedis()
+	} else {
+		fun = gftoken.WithGCache()
+	}
+	gT.GfToken = gftoken.NewGfToken(
+		gftoken.WithCacheKey(options.CacheKey),
+		gftoken.WithTimeout(options.Timeout),
+		gftoken.WithMaxRefresh(options.MaxRefresh),
+		gftoken.WithMultiLogin(options.MultiLogin),
+		gftoken.WithExcludePaths(options.ExcludePaths),
+		fun,
+	)
+	return &gT
+}
---
+++ internal/app/system/service/token.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"github.com/Rewriterl/xxManage/v1/internal/app/common/model"
+	commonService "github.com/Rewriterl/xxManage/v1/internal/app/common/service"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gctx"
+	"github.com/tiger1103/gfast/v3/library/liberr"
+	"sync"
+)
+
+type token struct {
+	options *model.TokenOptions
+	token   commonService.IToken
+	lock    *sync.Mutex
+}
+
+var tokenService = &token{
+	options: nil,
+	token:   nil,
+	lock:    &sync.Mutex{},
+}
+
+func Token() commonService.IToken {
+	if tokenService.token == nil {
+		tokenService.lock.Lock()
+		defer tokenService.lock.Unlock()
+		if tokenService.token == nil {
+			ctx := gctx.New()
+			err := g.Cfg().MustGet(ctx, "gfToken").Struct(&tokenService.options)
+			liberr.ErrIsNil(ctx, err)
+			tokenService.token = commonService.GfToken(tokenService.options)
+		}
+	}
+	return tokenService.token
+}
